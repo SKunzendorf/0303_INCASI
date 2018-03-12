abb6313cb98f3bf12bf69286a07ad4ad921a7d53
eioe <felix_klotzsche@web.de>
2018-03-12 01:52:35 +0100

    change load packages, setting of wd, link to data folder, bootstrap info

--- _scripts/loadPackages.R
+++ _scripts/loadPackages.R
@@ -1,27 +1,38 @@
 #--------------------------------------------------------------------------
-# load packages
-library(ggplot2)
-library(reshape)
-library(pracma)
-library(grid)
-library(ez)				# includes Greenhouse Geisser correction for ANOVA
-library(BayesFactor)
-library(dplyr)
-require(R.matlab)
-require(circular)
-library(plotrix)
-library(diptest)
-#require(RHRV)
-library(purrr)
-library(tidyr)
-library(lsr)
-library(effsize)
-library(plyr)
-library(gridExtra)
-library(cowplot)
-library(lme4)
-library(corrplot)
-library(mgcv)
+INCASI_packages <- c(
+  'ggplot2',
+  'reshape',
+  'pracma',
+  'pracma',
+  'grid',
+  'ez',				         # includes Greenhouse Geisser correction for ANOVA
+  'BayesFactor',       # if you're on windows, dependency 'doMC' will be installed but run sequentially;
+                       # bootstrapping will therefore take quite a while
+  'dplyr',
+  'R.matlab',
+  'circular',
+  'plotrix',
+  'diptest',
+  #'RHRV',
+  'purrr',
+  'tidyr',
+  'lsr',
+  'effsize',
+  'plyr',
+  'gridExtra',
+  'cowplot',
+  'lme4',
+  'corrplot',
+  'mgcv')
+
+
+for(pkg in INCASI_packages) {
+  if(!require(pkg, character.only = T)) {
+    install.packages(pkg, dependencies = T, repos="http://cran.rstudio.com/")
+  }
+}
+
+
 #--------------------------------------------------------------------------
 
 
